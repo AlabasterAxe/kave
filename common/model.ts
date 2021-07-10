3973206931ce00206bf46134b4fb3103b3ef1a79
Matthew Keller <matthew.ed.keller@gmail.com>
2021-07-10 13:04:06 -0400

    complete restructure of the data model to more closely align with descripts data model

--- common/model.ts
+++ common/model.ts
@@ -1,10 +1,9 @@
-export interface Clip {
-  id: string;
-  durationSeconds: number;
-  fileUri: string;
+export enum FileType {
+  // can be used as clipref
+  video,
 
-  // This is how many seconds from the start of the file that the timeline element starts.
-  fileOffsetSeconds: number;
+  // cannot be used as clipref by itself
+  interaction_log,
 }
 
 export interface UserInteraction {
@@ -13,13 +12,70 @@ export interface UserInteraction {
 }
 
 export interface UserInteractionLog {
-  // the alignment indicates where in the timeline (in seconds) the first user interaction should be placed.
-  alignment: number;
+  id: string;
   log: UserInteraction[];
 }
 
-export interface Timeline {
+export interface FileBase {
+  id: string;
+  type: FileType;
+
+  fileUri?: string;
+
+  // this is slightly gross. I'd like to have a more generic way to represent the user interaction log.
+  userInteractionLog?: UserInteractionLog;
+}
+
+export interface VideoFile extends FileBase {
+  type: FileType.video;
+  fileUri: string;
+}
+
+export interface InteractionLogFile extends FileBase {
+  type: FileType.interaction_log;
+  userInteractionLog: UserInteractionLog;
+}
+
+export type File = VideoFile | InteractionLogFile;
+
+export interface Track {
+  id: string;
+  fileId: string;
+
+  // the alignment of this file relative to the sequence, must be positive.
+  alignmentSeconds: number;
+}
+
+export interface Sequence {
+  id: string;
+  tracks: Track[];
+}
+
+export interface Clip {
+  id: string;
+  durationSeconds: number;
+
+  // This can be a video file or a sequence.
+  sourceId: string;
+
+  // This is how many seconds from the start of the file that the timeline element starts.
+  sourceOffsetSeconds: number;
+}
+
+export interface Composition {
+  id: string;
   clips: Clip[];
   durationSeconds: number;
-  userInteractions: UserInteractionLog;
+}
+
+export interface Project {
+  id: string;
+  files: File[];
+  sequences: Sequence[];
+  compositions: Composition[];
+}
+
+export interface TimelineViewport {
+  startTimeSeconds: number;
+  endTimeSeconds: number;
 }
